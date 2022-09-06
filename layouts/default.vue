c164942db765c9e41c4047e791499bff85cf7dab
mistadrumma <breakroger104@gmail.com>
2022-09-06 10:22:34 +0300

    create prj2

---
+++ layouts/default.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <Navbar />
+
+    <section class="main-content columns">
+<!--      <aside class="column is-2 section">-->
+<!--        <p class="menu-label is-hidden-touch">-->
+<!--          General-->
+<!--        </p>-->
+<!--        <ul class="menu-list">-->
+<!--          <li-->
+<!--            v-for="(item, key) of items"-->
+<!--            :key="key"-->
+<!--          >-->
+<!--            <NuxtLink-->
+<!--              :to="item.to"-->
+<!--              exact-active-class="is-active"-->
+<!--            >-->
+<!--              <b-icon :icon="item.icon" /> {{ item.title }}-->
+<!--            </NuxtLink>-->
+<!--          </li>-->
+<!--        </ul>-->
+<!--      </aside>-->
+
+      <div class="container column is-10">
+        <Nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Navbar from "~/components/Navbar";
+export default {
+  components: {Navbar},
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="section">
+ <h1>Горячие</h1>
+  </section>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomePage',
+
+  components: {
+
+  }
+}
+</script>
